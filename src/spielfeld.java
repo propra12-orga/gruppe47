5d8192b3b51f98af8487b85edba6d247b2911874
Timon Guenther <timon.guenther@uni-duesseldorf.de>
2012-05-08 19:02:48 +0200

    Ausgabe gefixed, Methode fuer zufaellige Mauern hinzugefuegt, kommentare zum verstaendnis hinzugefuegt

--- src/spielfeld.java
+++ src/spielfeld.java
@@ -8,6 +8,7 @@ public class spielfeld {
 	char[][] spielfeld = new char [hoehe][breite];
 	String begrenzung = "#";
 	erzeuge(hoehe,breite,spielfeld,begrenzung);
+	mauern(hoehe,breite,spielfeld, begrenzung);
 	darstelle(hoehe,breite,spielfeld);
 	}
 	public static void erzeuge(int hoehe, int breite, char [][] spielfeld, String begrenzung){
@@ -17,14 +18,23 @@ public class spielfeld {
 			{
 				if (i == 0 | i == (hoehe-1) )
 				{
-					spielfeld [i][j] = begrenzung.charAt(0);
+					spielfeld [i][j] = begrenzung.charAt(0); // Erzeugung der oberen und unteren begrenzung
 				}
-				if (j == 1 | j == (breite-1))
+				if (j == 0 | j == (breite-1))
 				{
-					spielfeld [i][j] = begrenzung.charAt(0);
+					spielfeld [i][j] = begrenzung.charAt(0); // Erzeugung der rechten und linken begrenzung
 				}
 
 			}
+		}
+	}
+	public static void mauern (int hoehe, int breite, char [][] spielfeld, String begrenzung)
+	{
+		for (int i=0; i < (hoehe*breite/3);i++) //1/3 des spielfeldes sollen mauern enthalten (wert noch nciht endgültig)
+		{
+			int h = (int) (Math.random() * ((hoehe - 1) - 1) + 1); //Zufallszahl für die Höhe
+			int b = (int) (Math.random() * ((breite - 1) -1) + 1); // Zufallszahl für die Breite
+			spielfeld [h][b] = begrenzung.charAt(0);
 		}
 	}
 	public static void darstelle(int hoehe, int breite, char [][] spielfeld)
